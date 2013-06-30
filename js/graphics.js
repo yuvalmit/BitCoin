374a237cee9c876fbea76a01ea7697af23723336
Yuval Mitrani <yuvalmit@gmail.com>
2013-06-30 11:13:52 +0300

    rope fight

---
+++ js/graphics.js
@@ -0,0 +1,185 @@
+
+
+
+var width = 960;
+var height = 3500;
+var pi = Math.PI;
+var timer;
+var items = [];
+var cur_index = 0;
+var max_index = 0;
+var tickTime = 10;
+
+
+var x = d3.scale.linear()
+        .domain([0, 200])
+        .range([1, 600]);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var usdRect = svg.append("rect")
+    .attr("class", "usdRect")
+    .attr("x", 100)
+    .attr("y", 100)
+    .attr("height", 30)
+    .attr("width", 650)
+    .attr("fill", "blue");
+
+var bitRect = svg.append("rect")
+    .attr("class", "bitRect")
+    .attr("x", 100)
+    .attr("y", 100)
+    .attr("height", 30)
+    .attr("width", 0)
+    .attr("fill", "green");
+
+var text = svg.append("text")
+    .attr("class","text")
+    .attr("x", 300)
+    .attr("y", 300)
+    .attr("font-size", "20px")
+    .text("");
+
+var num = svg.append("text")
+    .attr("class","text")
+    .attr("x", 100)
+    .attr("y", 50)
+    .attr("font-size", "20px")
+    .text("");
+
+var bitImge = svg.append("svg:image")
+      .attr("xlink:href", "images/b5.png")
+      .attr("x", "20")
+      .attr("y", "60")
+      .attr("width", "100")
+      .attr("height", "100");
+
+var usdImge = svg.append("svg:image")
+      .attr("xlink:href", "images/u5.png")
+      .attr("x", "730")
+      .attr("y", "60")
+      .attr("width", "100")
+      .attr("height", "100");
+
+$.getJSON("usd.json", function(data){
+
+  $.each(data, function(key, val) 
+  {
+    items.push(val);
+  });
+
+})
+.done(function() { init(); })
+
+
+function init()
+{
+  max_index = items.length-1;
+  start();
+
+}
+
+function changeRec()
+{
+  var size = items[cur_index].open;
+  var title = items[cur_index].date;
+  bitRect 
+    .transition()
+    .duration(tickTime)
+    .attr("width", function(){return 5+x(size)});
+    
+  text
+    .transition()
+    .duration(tickTime)
+    .text(function(){return title});
+
+  num
+    .transition()
+    .duration(tickTime)
+    .text(function(){return size});
+
+  var rand_numb = Math.round(Math.random() * (5 - 1))+1;
+  var rand_numu = 6 - rand_numb;
+  if(cur_index % 2 == 0)
+  { 
+    bitImge
+      .attr("xlink:href", "images/b"+ rand_numb +".png");
+   
+    usdImge
+      .attr("xlink:href", "images/u"+ rand_numu +".png");
+  }
+
+
+}
+
+function start() 
+{
+
+    changeRec();
+    if(cur_index < max_index)
+    {
+      timer = setTimeout(start, tickTime); 
+    }
+    else
+    {
+      stop();
+    }
+    cur_index++;      
+};
+
+function stop() {
+
+};
+
+
+
+
+/*console.log(data);
+for (var i = 0; i < data.length - 1;  i++) 
+  {
+    console.log(data[i]);
+
+
+    bitRect 
+    .transition()
+    .duration(1000)
+    .delay(100)  
+    .attr("width", function(){return data[i].high*10});
+
+
+    text
+    .transition()
+    .duration(1000)
+    .delay(100)  
+    .text(function(){return data[i].date});
+
+  };*/
+
+
+
+//kipod code
+/*var theData = []
+for (var i = 0; i<3500; i++)
+   theData[i] = i;
+
+var ang = 360 / theData.length;
+console.log(theData);
+
+var arc = d3.svg.arc()
+    .startAngle(function(i) { return (i*ang)* (pi/180); })
+    .endAngle(function(i) { return ((i*ang)+0.1)* (pi/180); })
+    .innerRadius(function(i) { return 10; })
+    .outerRadius(function(i) { return 250+Math.round(Math.random() * 100); })
+
+
+var p = svg.selectAll("p")
+  .data(theData)
+  .enter()
+  .append("path")
+  .attr("class","p")
+  .attr("d",arc)
+  .attr("transform", "translate(500,400)")
+  .style("fill", function(){return get_random_color();});*/
+
